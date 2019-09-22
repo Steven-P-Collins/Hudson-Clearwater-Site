2bf87f352e41028f1771e4125ddd2cc27326a6f2
Steven <stecolli2@publicisgroupe.net>
2019-09-22 14:21:42 -0400

    Added missing files

---
+++ js/menu_loader.js
@@ -0,0 +1,18 @@
+breakfast = {
+    "time": "Weekdays",
+    "bev": [
+        {
+            "veg smooth": [
+                {"super fruit": ["orange, abana, blue", "9"]},
+                {"CHoc alm": ["raw coc, baban, alm butt, alm mik", "9"]},
+                {"supp:" : ["add chia, maca, moringa, cbd, or orac", "3"]}
+            ]
+        }
+    ]
+};
+
+// const myJSON = JSON.stringify(breakfast);
+
+
+
+console.log(breakfast.bev[0]["veg smooth"][0]["super fruit"][0]);
