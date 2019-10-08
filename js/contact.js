fc3dbc9da6f1e7ce324f63800ec2d55a5dc4e5b9
Steven <stecolli2@publicisgroupe.net>
2019-10-08 14:29:38 -0400

    Site is ready. Have to make some minor changes, but it is ready to go live

--- js/contact.js
+++ js/contact.js
@@ -1,9 +1,18 @@
 window.onload = () => {
 
-    document.getElementById('date').addEventListener('input', e => {
+    setDates();
+
+    let date = document.getElementById('date');
+    // let time = document.getElementById('time');
+
+    date.addEventListener('input', e => {
         let date = e.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,2})(\d{0,4})/);
         e.target.value = !date[2] ? date[1] : date[1] + '/' + date[2] + (date[3] ? '/' + date[3] : '');
     });
+
+    // date.addEventListener('change', e => {
+    //     console.log(date.value);
+    // });
 
     document.getElementById('guests').addEventListener('input', e => {
         let guests = e.target.value.replace(/\Dg/, '').match(/(\d?)(\d?)/);
@@ -15,27 +24,23 @@ window.onload = () => {
         e.target.value = !phone[2] ? phone[1] : '(' + phone[1] + ') ' + phone[2] + (phone[3] ? '-' + phone[3] : '');
     });
 
-
-    $('#time').timepicker({
-        'disableTextInput': true,
-        'forceRoundTime': true,
-        'maxTime': '11:30PM',
-        'minTime': '7:30AM',
-        'selectOnBlur': true,
-        'step': 15,
-        }
-    );
-
-    $('#date').datepicker({
-        'minDate': new Date()
-    });
-
-    setDates();
-
 };
 
 setDates = () => {
-    let today = new Date();
+
+    let date = $('#date'),
+        time = $('#time'),
+        today = new Date();
+
+    time.timepicker({
+            'disableTextInput': true,
+            'forceRoundTime': true,
+            'maxTime': '11:30PM',
+            'minTime': '7:30AM',
+            'selectOnBlur': true,
+            'step': 15,
+        }
+    );
 
     if (today.getHours() < 7 || (today.getHours() === 7 && today.getMinutes() < 30)) {
         $('#time').timepicker(
@@ -43,12 +48,18 @@ setDates = () => {
         )
     }
     else {
-        $('#time').timepicker(
+        time.timepicker(
             'setTime', today
         );
     }
 
-    $('#date').datepicker(
+    date.datepicker({
+        'minDate': today,
+        'maxDate': '+6m'
+    });
+
+
+    date.datepicker(
         'setDate', today
     );
 
